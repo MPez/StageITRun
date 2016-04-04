1f137d55a04afd80ee3e3185921adeb44fe0a634
Marco Pezzutti <marco.pezzutti@hotmail.it>
2016-04-04 18:48:31 +0200

    aggiornati json

--- app/src/main/java/it/unipd/mpezzutt/stageitrun/MainActivity.java
+++ app/src/main/java/it/unipd/mpezzutt/stageitrun/MainActivity.java
@@ -7,13 +7,20 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ListView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -76,11 +83,38 @@ public class MainActivity extends AppCompatActivity {
             input.read(buffer);
             input.close();
             json = new String(buffer, "UTF-8");
-        } catch (IOException ex) {
-            ex.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
             return json;
         }
 
         return json;
+    }
+
+    public void setJSONArray (String asset, String objName) {
+        try {
+            JSONObject jObj = new JSONObject(loadJSONFromAsset(asset));
+            JSONArray jArray = jObj.getJSONArray(objName);
+            HashMap<String, String> jMap;
+            ArrayList<HashMap<String, String>> jList = new ArrayList<HashMap<String, String>> ();
+
+            for (int i = 0; i < jArray.length(); i++) {
+                JSONObject jArrayObj = jArray.getJSONObject(i);
+                Log.d("Stage ->> ", jArrayObj.getString("nome") + jArrayObj.getString("azienda"));
+                String nomeStage = jArrayObj.getString("nome");
+                String nomeAzienda = jArrayObj.getString("azienda");
+                String descrAzienda = jArrayObj.getString("descrizione");
+
+                //Add your values in your `ArrayList` as below:
+                jMap = new HashMap<String, String>();
+                jMap.put("formule", nomeStage);
+                jMap.put("url", nomeAzienda);
+
+                jList.add(jMap);
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
